295fb9ee1a96e69f487fc8a582ab95942f0d803f
Everton <evertomcdg007@hotmail.com>
2016-12-17 08:40:31 -0300

    progressbar e chronometer com lógica simples do pomodora

--- app/src/main/java/com/infinitybreak/tumate/activities/MainActivity.java
+++ app/src/main/java/com/infinitybreak/tumate/activities/MainActivity.java
@@ -1,17 +1,43 @@
 package com.infinitybreak.tumate.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ProgressBar;
 
 import com.infinitybreak.tumate.R;
 
+import library.minimize.com.chronometerpersist.ChronometerPersist;
+
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chronometer;
+    Button buttonStart;
+    Button buttonStop;
+    ProgressBar progressBar;
+    ChronometerPersist chronometerPersist;
+    SharedPreferences sharedPreferences;
+    int stopCoffeeBreak = 0;
+    int progressStatus = 0;
+    int progress = 0;
+    public static int TIME_MAX = 15;
+
+    AsyncTask asyncTask;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,120 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        progressBar = (ProgressBar) findViewById(R.id.circle_progress_bar);
+
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        buttonStart = (Button) findViewById(R.id.start_button);
+        buttonStop = (Button) findViewById(R.id.stop_button);
+
+        sharedPreferences = getSharedPreferences("ChronometerSample", MODE_PRIVATE);
+        chronometerPersist = ChronometerPersist.getInstance(chronometer, sharedPreferences);
+
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initThreadProgressBar(0, TIME_MAX);
+                chronometerPersist.startChronometer();
+            }
+        });
+
+
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopChronometer();
+            }
+        });
+
+
+
+    }
+
+    public void startChronometer(){
+        chronometerPersist.startChronometer();
+        initThreadProgressBar(0, TIME_MAX);
+    }
+    public void stopChronometer(){
+        chronometerPersist.stopChronometer();
+        progressBar.setProgress(0);
+    }
+
+    public void updateView(){
+
+
+    }
+
+
+    /*
+    * Se 1000 mili segundo é 1 segundo
+    * e 1 min é 60 segundo
+    * então 25 min é 1500s
+    * e 5 min é 300s
+    *
+    * */
+    public void initThreadProgressBar(int timeCurrent, final int timeMax){
+
+        progressStatus = timeCurrent;
+        progressBar.setMax(timeMax);
+
+        asyncTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(final Void... params) {
+                while (progressStatus < TIME_MAX) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    progressStatus ++;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                }
+                handler.post(new Runnable() {
+                    public void run() {
+                        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                        v.vibrate(500);
+
+                        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+                        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+
+                        chronometerPersist.stopChronometer();
+                        if(stopCoffeeBreak % 4 == 0){
+                            TIME_MAX = 10;
+                        }else if(stopCoffeeBreak % 2 == 0){
+                            TIME_MAX = 5;
+                        } else if(stopCoffeeBreak % 2 != 0){
+                            TIME_MAX = 15;
+                        }
+
+
+                    }
+                });
+                return null;
+            }
+        }.execute();
+
+        stopCoffeeBreak++;
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        chronometerPersist.resumeState();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 
     @Override
